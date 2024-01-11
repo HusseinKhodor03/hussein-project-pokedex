bf75737bc08a2e1672b35ca044679a280473a30e
Hussein Khodor <hkhodor03@gmail.com>
2024-01-11 01:27:48 -0800

    Added more specific error messages based on the type of error. Also reconfigured "retry" react query setting

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,13 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,8 +1,15 @@
-import { useNavigate } from "react-router-dom";
+import {
+  isRouteErrorResponse,
+  useNavigate,
+  useParams,
+  useRouteError,
+} from "react-router-dom";
 import "../styles/ErrorPage.css";
 
 function ErrorPage() {
   const navigate = useNavigate();
+  const error = useRouteError();
+  const name = useParams();
 
   return (
     <>
@@ -10,7 +17,9 @@ function ErrorPage() {
         <h2 className="error__heading">Oops! An error occurred...</h2>
         <div className="error__image"></div>
         <p className="error__text">
-          The page you were looking for was not found =(
+          {isRouteErrorResponse(error)
+            ? "The page you were looking for was not found =("
+            : `We didn't find any Pokémon with this filter: "${name.name}"`}
         </p>
         <button className="error__btn" onClick={() => navigate("/")}>
           Return to Home Page
