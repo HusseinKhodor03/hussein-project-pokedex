2d856a579e87ea6f4d5860398776c99e5b661233
Hussein Khodor <hkhodor03@gmail.com>
2024-01-17 13:53:54 -0800

    Updated the loading spinner to animate the ellipsis at the end of the loading heading tag

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import "../styles/LoadingSpinner.css";
 function LoadingSpinner() {
   return (
     <div>
-      <h2 className="loader-title">Loading...</h2>
+      <h2 className="loader-title">Loading</h2>
       <div className="loader"></div>
     </div>
   );
