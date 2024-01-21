8f88fb4140955d658b6c3bc7d6649a31fb024db1
Hussein Khodor <hkhodor03@gmail.com>
2024-01-20 16:25:53 -0800

    Testing for previously mentioned bug

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import "./normalize.css";
+// import "./normalize.css";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
